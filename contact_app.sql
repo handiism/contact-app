532d4857fbd096e509b65924f97037a6bfdbede4
handiism <mail.handira@gmail.com>
2022-05-17 15:06:26 +0700

    update

---
+++ contact_app.sql
@@ -0,0 +1,83 @@
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: contact_app
+-- ------------------------------------------------------
+-- Server version	10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone` (`phone`),
+  KEY `fk_user_id` (`user_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contact`
+--
+
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+INSERT INTO `contact` VALUES (1,'namebaru','phonebaru',3),(3,'eman','enohp',3),(4,'paijo','1308013',1),(5,'kontak1','1380130',5),(6,'kontak2','731013',5),(7,'kontak3','013801398',5);
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'supri','13803180','password'),(3,'tukiman','130i0','tukino'),(5,'paijem','99999','paijem');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-17 15:05:50
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import entity.Contact;
 import entity.User;
 import java.util.ArrayList;
 import java.util.List;
-
 import repository.contact.ContactRepository;
 import repository.contact.ContactRepositoryImpl;
 import repository.user.UserRepository;
@@ -11,42 +10,24 @@ import repository.user.UserRepositoryImpl;
 
 public class Main {
   public static void main(String[] args) {
-    MySQL mySQL = new MySQL();
-    UserRepository userRepository =
-        new UserRepositoryImpl(mySQL.getConnection());
-    // register
-    // boolean success =
-    //     userRepository.register(new User("paijem", "99999", "paijem"));
-    // if (success) {
-    //   System.out.println("Sukses Daftar");
-    // }
+    // tersedia menu login dan register
 
-    // login
-    User user = userRepository.login(new User("paijem", "99999", "paijem"));
-    if (user == null) {
-      System.out.println("Salah");
-    } else {
+    // login memakai nomer hp dan password
+    // register memakai nomer hp, nama, dan password
 
-      ContactRepository contactRepository =
-          new ContactRepositoryImpl(mySQL.getConnection(), user);
-      // nambah kontak berdasarkan user sik login
-      // contactRepository.add(new Contact("kontak1", "1380130"));
-      // contactRepository.add(new Contact("kontak2", "731013"));
-      // contactRepository.add(new Contact("kontak3", "013801398"));
-      List<Contact> contacs = contactRepository.readAll();
-      for (Contact contact : contacs) {
-        // System.out.println(contact.getId());
-        // System.out.println(contact.getName());
-        // System.out.println(contact.getPhone());
-        // System.out.println();
-      }
+    // jika gagal register, maka terdapat menu akan mengulang atau
+    // kembali ke menu login/register
 
-      // Contact contoh = contactRepository.read(5);
-      // System.out.println(contoh.getId());
-      // System.out.println(contoh.getName());
-      // System.out.println(contoh.getPhone());
-      // contactRepository.update(new Contact(1, "namebaru", "phonebaru"));
-      // contactRepository.delete(new Contact(2, "namename", "phonephone"));
-    }
+    // jikalogin gagal maka terdapat menu untuk kembali login atau kembali ke
+    // menu login/register
+
+    // jika register berhasil, maka kembali ke menu login/register jika login
+    // berhasil, maka terdapat menu lihat contact, ubah kontak, cari kontak,
+    // hapus kontak
+
+    // tambah kontak memakai nomer hp dan nama
+    // edit kontak memakai id konttak, nomer hp, dan nama
+    // hapus memakai id kontak
+    // cari kontak memakai id kontak
   }
 }
