1a4b1d4043ff4ff115bbbe8cc120af34aee5f85b
handiism <mail.handira@gmail.com>
2022-05-18 00:57:13 +0700

    hapus komentar

--- src/Main.java
+++ src/Main.java
@@ -10,26 +10,6 @@ import repository.user.UserRepositoryImpl;
 
 public class Main {
   public static void main(String[] args) {
-    // tersedia menu login dan register
-
-    // login memakai nomer hp dan password
-    // register memakai nomer hp, nama, dan password
-
-    // jika gagal register, maka terdapat menu akan mengulang atau
-    // kembali ke menu login/register
-
-    // jikalogin gagal maka terdapat menu untuk kembali login atau kembali ke
-    // menu login/register
-
-    // jika register berhasil, maka kembali ke menu login/register jika login
-    // berhasil, maka terdapat menu lihat contact, ubah kontak, cari kontak,
-    // hapus kontak
-
-    // tambah kontak memakai nomer hp dan nama
-    // edit kontak memakai id konttak, nomer hp, dan nama
-    // hapus memakai id kontak
-    // cari kontak memakai id kontak
-
     Scanner scanner = new Scanner(System.in);
 
     MySQL mySQL = new MySQL();
